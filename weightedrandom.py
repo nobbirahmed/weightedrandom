1d4f1aad95f7fc9eff09e716ac55793c1ce65242
nobbirahmed <nobbirahmed@gmail.com>
2017-05-16 16:22:39 -0700

    deleted codes related to reading from url

--- weightedrandom.py
+++ weightedrandom.py
@@ -4,16 +4,6 @@ import sys, os
 
 num_of_choices = 27
 
-#from urllib2 import urlopen
-#data = urlopen(r"http://arczone.esri.com/")
-#contents = data.readlines()
-#output = open(os.path.join(os.getcwd(), "numbers.txt"), "w")
-#for line in contents:
-    #print("Writing %s to a file" % (line,))
-    #output.write(line)
-    
-#sys.exit()
-# items to be populated from web
 items = [12, 22, 9, 24, 27, 21, 15, 18, 1, 19, 3, 26, 10, 16, 5, 25, 20, 6, 7, 4, 11, 23, 8, 13, 14, 17, 2]
 
 weights = []  # subjective
